d844da9c64105737fcfa07358e76ea90c759b045
Jon Carifio <carifio24@gmail.com>
2020-01-08 17:53:13 -0500

    Added a versions folder, which really should've been there from the start. Added scripts for creating APKS files from AABs, and for deploying an APK from the APKS to a connected device.

---
+++ versions/Commands.sh
@@ -0,0 +1,4 @@
+versionCode=20702
+
+# For building the APKs
+bundletool build-apks --bundle=/home/jon/git/SpellbookApp/AAB/${versionCode}.aab --output=/home/jon/git/SpellbookApp/${versionCode}.apks --ks=/home/jon/keystores/android_key.jks --ks-pass=C@rifio00 --ks-key-alias=JonsKey --key-pass=C@rifio00
