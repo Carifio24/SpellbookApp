454d5263ebc21807a96083874f4f9207ab4aafb8
Carifio24 <carifio24@gmail.com>
2020-02-12 01:24:53 -0500

    Some minor improvements.

--- app/src/main/java/dnd/jon/spellbook/OnSwipeTouchListener.java
+++ app/src/main/java/dnd/jon/spellbook/OnSwipeTouchListener.java
@@ -35,8 +35,12 @@ public class OnSwipeTouchListener implements View.OnTouchListener {
 
         @Override
         public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            float distanceX = e2.getX() - e1.getX();
-            float distanceY = e2.getY() - e1.getY();
+
+            // Null check
+            if ( (e1 == null) || (e2 == null) ) { return false; }
+
+            final float distanceX = e2.getX() - e1.getX();
+            final float distanceY = e2.getY() - e1.getY();
             if (Math.abs(distanceX) > Math.abs(distanceY) && Math.abs(distanceX) > SWIPE_DISTANCE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
                 if (distanceX > 0)
                     onSwipeRight();
