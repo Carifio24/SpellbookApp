fcab8e48b1ead30c71b907a323b713543e4a424d
Jon Carifio <carifio24@gmail.com>
2021-02-03 19:12:04 -0500

    Removed previous built releases from repository.
    These have been moved to their own repo.

--- versions/DeployAPK.sh
+++
@@ -1,7 +0,0 @@
-versionCode=${1}
-BUNDLETOOL="bundletool-all-1.4.0.jar"
-BUNDLETOOL_CMD="java -jar ${BUNDLETOOL}"
-ADB="/home/jon/Android/Sdk/platform-tools/adb"
-
-# For deploying
-${BUNDLETOOL_CMD} install-apks --adb ${ADB} --apks=${PWD}/APKS/${versionCode}.apks
