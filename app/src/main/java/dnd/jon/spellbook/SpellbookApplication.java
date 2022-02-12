167c0fb30645656f90a775e609ec420719070b89
Carifio24 <carifio24@gmail.com>
2022-02-12 18:30:04 -0500

    Override the default Application class to always keep dark mode turned off regardless of phone settings.

---
+++ app/src/main/java/dnd/jon/spellbook/SpellbookApplication.java
@@ -0,0 +1,15 @@
+package dnd.jon.spellbook;
+
+import android.app.Application;
+
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class SpellbookApplication extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+}
