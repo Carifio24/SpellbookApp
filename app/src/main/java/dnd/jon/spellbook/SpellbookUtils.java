7db0d0f9b06560fe175d9455b9b490fdcadf81d7
Jon Carifio <carifio24@gmail.com>
2022-09-03 19:30:17 -0400

    Fix ordinal function (NB: already fixed in v2).

--- app/src/main/java/dnd/jon/spellbook/SpellbookUtils.java
+++ app/src/main/java/dnd/jon/spellbook/SpellbookUtils.java
@@ -110,6 +110,8 @@ class SpellbookUtils {
                 return "1st";
             case 2:
                 return "2nd";
+            case 3:
+                return "3rd";
             default:
                 return value + "th";
         }
