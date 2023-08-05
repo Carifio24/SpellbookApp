e26049ca8c07602fb878b254f067de08326c007b
Jon Carifio <carifio24@gmail.com>
2023-08-05 14:08:12 -0400

    Do some updating of adapters and display utilities to account for non-enum (i.e. Source) cases.

--- app/src/main/java/dnd/jon/spellbook/NamedSpinnerAdapter.java
+++ app/src/main/java/dnd/jon/spellbook/NamedSpinnerAdapter.java
@@ -21,9 +21,9 @@ class NamedSpinnerAdapter<T extends Enum<T>> extends DefaultSpinnerAdapter<T> {
     private final Class<T> type;
 
     NamedSpinnerAdapter(Context context, Class<T> type, BiFunction<Context,T,String> namingFunction, int textSize) {
-        super(context, type.getEnumConstants(), namingFunction);
+        super(context, type.getEnumConstants(), namingFunction, textSize);
         this.type = type;
-    }
+   }
 
     NamedSpinnerAdapter(Context context, Class<T> type, BiFunction<Context,T,String> namingFunction) { this(context, type, namingFunction,12); }
 
