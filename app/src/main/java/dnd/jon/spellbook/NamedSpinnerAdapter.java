7496dff2a339247878cbe17f71e73a569fb3daa6
Jon Carifio <carifio24@gmail.com>
2023-08-12 19:20:05 -0400

    Update logic in spinner adapters.

--- app/src/main/java/dnd/jon/spellbook/NamedSpinnerAdapter.java
+++ app/src/main/java/dnd/jon/spellbook/NamedSpinnerAdapter.java
@@ -1,7 +1,16 @@
 package dnd.jon.spellbook;
 
 import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+
+import java.util.Arrays;
 import java.util.function.BiFunction;
 
 class NamedSpinnerAdapter<T extends Enum<T>> extends DefaultSpinnerAdapter<T> {
@@ -12,7 +21,7 @@ class NamedSpinnerAdapter<T extends Enum<T>> extends DefaultSpinnerAdapter<T> {
     private final Class<T> type;
 
     NamedSpinnerAdapter(Context context, Class<T> type, BiFunction<Context,T,String> namingFunction, int textSize) {
-        super(context, type.getEnumConstants(), namingFunction, textSize);
+        super(context, type.getEnumConstants(), namingFunction);
         this.type = type;
     }
 
