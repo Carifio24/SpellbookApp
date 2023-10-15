471ae91fa3343a355b5baa106fd418709662bf30
Jon Carifio <carifio24@gmail.com>
2023-10-15 02:47:23 -0400

    Add update text and bump version info.

--- app/src/main/java/dnd/jon/spellbook/GlobalInfo.java
+++ app/src/main/java/dnd/jon/spellbook/GlobalInfo.java
@@ -2,16 +2,16 @@ package dnd.jon.spellbook;
 
 class GlobalInfo {
 
-    static final Version VERSION = new Version(3,3,0);
+    static final Version VERSION = new Version(3,4,0);
     static final String VERSION_CODE = VERSION.string();
 
     // We don't always want to show an update message
     // i.e. for updates that are pure bugfixes, the old message may be
     // more useful to users
-    static final Version UPDATE_LOG_VERSION = new Version(3,3,0);
+    static final Version UPDATE_LOG_VERSION = new Version(3,4,0);
     static final String UPDATE_LOG_CODE = UPDATE_LOG_VERSION.string();
-    static final int UPDATE_LOG_TITLE_ID = R.string.update_03_03_00_title;
-    static final int UPDATE_LOG_DESCRIPTION_ID = R.string.update_03_03_00_description;
+    static final int UPDATE_LOG_TITLE_ID = R.string.update_03_04_00_title;
+    static final int UPDATE_LOG_DESCRIPTION_ID = R.string.update_03_04_00_description;
 
     static final int ANDROID_VERSION = android.os.Build.VERSION.SDK_INT;
 
