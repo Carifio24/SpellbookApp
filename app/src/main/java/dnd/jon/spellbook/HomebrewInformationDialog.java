cc5b2f32d2c3af6ee4a50f47fe632f3682eadf9b
Carifio24 <carifio24@gmail.com>
2024-03-08 02:09:26 -0500

    Finish homebrew information string resource. Set maximum height of homebrew information dialog to be 80% of the screen height.

--- app/src/main/java/dnd/jon/spellbook/HomebrewInformationDialog.java
+++ app/src/main/java/dnd/jon/spellbook/HomebrewInformationDialog.java
@@ -3,6 +3,9 @@ package dnd.jon.spellbook;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -22,6 +25,18 @@ public class HomebrewInformationDialog extends DialogFragment {
 
         final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setView(binding.getRoot());
-        return builder.create();
+        final AlertDialog dialog = builder.create();
+
+        // Set the maximum height of the dialog to be 80% of the screen height
+        final DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        final WindowManager.LayoutParams params = new WindowManager.LayoutParams();
+        final Window window = dialog.getWindow();
+        params.copyFrom(window.getAttributes());
+        params.height = (int) Math.max(params.height, (int) metrics.widthPixels * 0.8f);
+        window.setAttributes(params);
+
+        return dialog;
     }
+
 }
