fc2e4eadc18f743e5dbf91bf332af8e256076fe5
Carifio24 <carifio24@gmail.com>
2024-05-26 15:53:31 -0400

    Add button to spell creation dialog to allow deleting spells on a tablet.

--- app/src/main/java/dnd/jon/spellbook/SpellCreationDialog.java
+++ app/src/main/java/dnd/jon/spellbook/SpellCreationDialog.java
@@ -5,6 +5,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -15,6 +16,7 @@ import dnd.jon.spellbook.databinding.SpellCreationBinding;
 
 public class SpellCreationDialog extends DialogFragment {
     private static final String TAG = "SpellCreationDialog";
+    private static final String DELETE_SPELL_DIALOG_TAG = "DeleteSpellDialog";
     private static final String SPELL_KEY = "spell";
     private SpellbookViewModel viewModel;
     private SpellCreationHandler handler;
@@ -38,11 +40,23 @@ public class SpellCreationDialog extends DialogFragment {
 
         final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         final LayoutInflater inflater = getLayoutInflater();
-        final SpellCreationBinding binding  = SpellCreationBinding.inflate(inflater);
+        final SpellCreationBinding binding = SpellCreationBinding.inflate(inflater);
         handler = new SpellCreationHandler(activity, binding, TAG, spell);
         handler.setOnSpellCreated(this::dismiss);
         handler.setup();
         builder.setView(binding.getRoot());
+        if (spell != null && spell.equals(editingSpell)) {
+            binding.deleteSpellButton.setVisibility(View.VISIBLE);
+            final Spell spellToDelete = spell;
+            binding.deleteSpellButton.setOnClickListener(v -> {
+                final DeleteSpellDialog dialog = new DeleteSpellDialog();
+                dialog.setOnConfirm(this::dismiss);
+                final Bundle args = new Bundle();
+                args.putString(DeleteSpellDialog.NAME_KEY, spellToDelete.getName());
+                dialog.setArguments(args);
+                dialog.show(activity.getSupportFragmentManager(), DELETE_SPELL_DIALOG_TAG);
+            });
+        }
         viewModel.currentEditingSpell().observe(requireActivity(), (newSpell) -> {
             if (newSpell != null) {
                 handler.setSpellInfo(newSpell);
