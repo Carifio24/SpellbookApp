fb20ff89bc526599728d736c87eef0421886ad9b
Carifio24 <carifio24@gmail.com>
2024-09-12 21:20:56 -0400

    Add tests for quantity parsing. Update spell tests.

--- app/src/test/java/dnd/jon/spellbook/DurationTest.java
+++ app/src/test/java/dnd/jon/spellbook/DurationTest.java
@@ -1,2 +1,135 @@
-package dnd.jon.spellbook;public class DurationTest {
+package dnd.jon.spellbook;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.google.common.truth.Truth;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+public class DurationTest {
+
+    private Context context;
+
+    @Before
+    public void setup() {
+        context = InstrumentationRegistry.getInstrumentation().getContext();
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationInSecondsTest() {
+        Duration duration = new Duration(Duration.DurationType.SPANNING, 3, TimeUnit.ROUND);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(18);
+
+        duration = new Duration(Duration.DurationType.SPANNING, 8, TimeUnit.SECOND);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(8);
+
+        duration = new Duration(Duration.DurationType.SPANNING, 30, TimeUnit.MINUTE);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(1800);
+
+        duration = new Duration(Duration.DurationType.SPANNING, 3, TimeUnit.HOUR);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(10800);
+
+        duration = new Duration(Duration.DurationType.SPANNING, 2, TimeUnit.DAY);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(172800);
+
+        duration = new Duration(Duration.DurationType.SPANNING, 7, TimeUnit.YEAR);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(220752000);
+
+        duration = new Duration(Duration.DurationType.SPECIAL);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(0);
+
+        duration = new Duration(Duration.DurationType.INSTANTANEOUS);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(0);
+
+        duration = new Duration(Duration.DurationType.UNTIL_DISPELLED);
+        Truth.assertThat(duration.timeInSeconds()).isEqualTo(0);
+    }
+    @Test
+    @Config(sdk = 28)
+    public void DurationSpecialParseTest() {
+        final String durationString = "Special";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.SPECIAL);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationInstantaneousParseTest() {
+        final String durationString = "Instantaneous";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.INSTANTANEOUS);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationUntilDispelledParseTest() {
+        final String durationString = "Until dispelled";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.UNTIL_DISPELLED);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationSpanningParseTest() {
+        final String durationString = "3 seconds";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.SPANNING, 3, TimeUnit.SECOND);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationSpanningParseTest2() {
+        final String durationString = "5 rounds";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.SPANNING, 5, TimeUnit.ROUND);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+    @Test
+    @Config(sdk = 28)
+    public void DurationSpanningParseTest3() {
+        final String durationString = "2 hours";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.SPANNING, 2, TimeUnit.HOUR);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationSpanningParseTest4() {
+        final String durationString = "1 year";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.SPANNING, 1, TimeUnit.YEAR);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationSpanningParseTest5() {
+        final String durationString = "18 minutes";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.SPANNING, 18, TimeUnit.MINUTE);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void DurationSpanningParseTest6() {
+        final String durationString = "100 hours";
+        final Duration duration = DisplayUtils.durationFromString(context, durationString);
+        final Duration expected = new Duration(Duration.DurationType.SPANNING, 100, TimeUnit.HOUR);
+        Truth.assertThat(duration).isEqualTo(expected);
+    }
+
 }
--- app/src/test/java/dnd/jon/spellbook/RangeTest.java
+++ app/src/test/java/dnd/jon/spellbook/RangeTest.java
@@ -1,2 +1,134 @@
-package dnd.jon.spellbook;public class RangeTest {
+package dnd.jon.spellbook;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.google.common.truth.Truth;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@RunWith(RobolectricTestRunner.class)
+public class RangeTest {
+
+    private Context context;
+
+    @Before
+    public void setup() {
+        context = InstrumentationRegistry.getInstrumentation().getContext();
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeSpecialParseTest() {
+        final String rangeString = "Special";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.SPECIAL);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeSelfParseTest() {
+        final String rangeString = "Self";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.SELF);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeSelfParseTest2() {
+        final String rangeString = "Self (15 foot radius)";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.SELF, 15, LengthUnit.FOOT);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeSelfParseTest3() {
+        final String rangeString = "Self (15-foot cone)";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.SELF, 15, LengthUnit.FOOT);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeTouchParseTest() {
+        final String rangeString = "Touch";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.TOUCH);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeSightParseTest() {
+        final String rangeString = "Sight";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.SIGHT);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeUnlimitedParseTest() {
+        final String rangeString = "Unlimited";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.UNLIMITED);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeFiniteParseTest() {
+        final String rangeString = "1 foot";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.RANGED, 1, LengthUnit.FOOT);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeFiniteParseTest2() {
+        final String rangeString = "2 feet";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.RANGED, 2, LengthUnit.FOOT);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeFiniteParseTest3() {
+        final String rangeString = "1106 meters";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.RANGED, 1106, LengthUnit.METER);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeFiniteParseTest4() {
+        final String rangeString = "3 kilometers";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.RANGED, 3, LengthUnit.KILOMETER);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
+    @Test
+    @Config(sdk = 28)
+    public void RangeFiniteParseTest5() {
+        final String rangeString = "6 miles";
+        final Range range = DisplayUtils.rangeFromString(context, rangeString);
+        final Range expected = new Range(Range.RangeType.RANGED, 6, LengthUnit.MILE);
+        Truth.assertThat(range).isEqualTo(expected);
+    }
+
 }
