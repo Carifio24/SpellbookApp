3c51fde64b0881d50a1d6fb0f7ba56b52103e134
Carifio24 <carifio24@gmail.com>
2024-10-15 00:44:38 -0400

    Add utility function to create document opening Intent.

--- app/src/main/java/dnd/jon/spellbook/AndroidUtils.java
+++ app/src/main/java/dnd/jon/spellbook/AndroidUtils.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Build;
@@ -13,6 +14,8 @@ import android.provider.MediaStore;
 import android.provider.OpenableColumns;
 import android.util.DisplayMetrics;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
 
 import org.javatuples.Pair;
 
@@ -33,6 +36,18 @@ class AndroidUtils {
     public static boolean isExternalStorageAvailable() {
         String extStorageState = Environment.getExternalStorageState();
         return Environment.MEDIA_MOUNTED.equals(extStorageState);
+    }
+
+    static Intent openDocumentIntent(String fileType, String initialURI) {
+        final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType(fileType);
+
+        if (initialURI != null && OSUtils.getSDKInt() >= Build.VERSION_CODES.O) {
+            intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, initialURI);
+        }
+
+        return intent;
     }
 
 }
