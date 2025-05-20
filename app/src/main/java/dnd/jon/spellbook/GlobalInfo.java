68fe9c310b5958f7b99518a9d00a6884a1a53a52
Carifio24 <carifio24@gmail.com>
2025-05-20 01:13:54 -0400

    Update version info and update data for version 4.4.3.

--- app/src/main/java/dnd/jon/spellbook/GlobalInfo.java
+++ app/src/main/java/dnd/jon/spellbook/GlobalInfo.java
@@ -2,13 +2,13 @@ package dnd.jon.spellbook;
 
 class GlobalInfo {
 
-    static final Version VERSION = new Version(4,4,2);
+    static final Version VERSION = new Version(4,4,3);
     static final String VERSION_CODE = VERSION.string();
 
     // We don't always want to show an update message
     // i.e. for updates that are pure bugfixes, the old message may be
     // more useful to users
-    static final Version UPDATE_LOG_VERSION = new Version(4,4,2);
+    static final Version UPDATE_LOG_VERSION = new Version(4,4,3);
     static final String UPDATE_LOG_CODE = UPDATE_LOG_VERSION.string();
     static final int UPDATE_LOG_DESCRIPTION_ID = R.string.update_04_04_02_description;
 
