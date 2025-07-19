426568ab030fd393e4babf4c5d60ab92ce3c01e0
Carifio24 <carifio24@gmail.com>
2025-07-19 14:57:28 -0400

    Update dependencies. Setting insets to avoid system bars is working in the main activity, though the background color needs changing.

--- app/src/main/java/dnd/jon/spellbook/SpellbookActivity.java
+++ app/src/main/java/dnd/jon/spellbook/SpellbookActivity.java
@@ -2,8 +2,14 @@ package dnd.jon.spellbook;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 import androidx.preference.PreferenceManager;
 
 public class SpellbookActivity extends AppCompatActivity {
@@ -18,6 +24,11 @@ public class SpellbookActivity extends AppCompatActivity {
         // No need to recreate the theme here, as the content view hasn't been set up
         // In fact, if we do recreate, we end up in an endless loop
         updateTheme(theme, false);
+
+        // Apply edge-to-edge for the app content
+        // This is default on Android 15, so it seems easiest to enforce this everywhere
+        // and not have to maintain two layouts
+        EdgeToEdge.enable(this);
 
         super.onCreate(savedInstanceState);
     }
