80325d1357c01908db6754f26c3b265b585c6bba
Carifio24 <carifio24@gmail.com>
2025-07-28 01:34:35 -0400

    Bump version info for version 4.4.7.

--- app/src/main/java/dnd/jon/spellbook/GlobalInfo.java
+++ app/src/main/java/dnd/jon/spellbook/GlobalInfo.java
@@ -2,7 +2,7 @@ package dnd.jon.spellbook;
 
 class GlobalInfo {
 
-    static final Version VERSION = new Version(4,4,6);
+    static final Version VERSION = new Version(4,4,7);
     static final String VERSION_CODE = VERSION.string();
 
     // We don't always want to show an update message
@@ -10,7 +10,7 @@ class GlobalInfo {
     // more useful to users
     static final Version UPDATE_LOG_VERSION = new Version(4,4,5);
     static final String UPDATE_LOG_CODE = UPDATE_LOG_VERSION.string();
-    static final int UPDATE_LOG_DESCRIPTION_ID = R.string.update_04_04_04_description;
+    static final int UPDATE_LOG_DESCRIPTION_ID = R.string.update_04_04_05_description;
 
     static final int ANDROID_VERSION = android.os.Build.VERSION.SDK_INT;
 
